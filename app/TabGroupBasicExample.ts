061b542f77b22e9e1aebdba3ca93d2f0416a1651
sara0909 <55620977+sara0909@users.noreply.github.com>
2019-10-15 11:16:08 +0530

    Add files via upload

---
+++ app/TabGroupBasicExample.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'tab-group-basic-example',
+    templateUrl: 'tab-group-basic-example.html',
+    styleUrls: ['tab-group-basic-example.css'],
+})
+// tslint:disable-next-line: component-class-suffix
+export class TabGroupBasicExample {
+}
